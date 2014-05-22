2d8ae0e4d130aef69e6af00c05f17595285747ee
Andrew Hochstetter <ajh334@drexel.edu>
2014-05-22 11:21:50 -0400

    added the test hello world file

--- Makefile
+++ Makefile
@@ -9,5 +9,11 @@ modAVU:
 magicNumber:
         ${GCC} ${INC} -fPIC -shared -o libmsiMagicNumber.so magicNumberMS.cpp -Wno-deprecated /usr/lib/irods/$
 
+hello:
+        ${GCC} ${INC} -fPIC -shared -o libirods_hello.so irods_hello.cpp -Wno-deprecated /usr/lib/irods/libirods_client.a
+        ${GCC} ${INC} -fPIC -shared -o libirods_hello2.so irods_hello.cpp -Wno-deprecated /usr/lib/irods/libirods_client.a
+
+
+
 clean:
 	@rm -f libmodAVUMetadataMS.so
---
+++ irods_hello.cpp
@@ -0,0 +1,109 @@
+// =-=-=-=-=-=-=-
+#include "msParam.hpp"
+#include "reGlobalsExtern.hpp"
+#include "irods_ms_plugin.hpp"
+#include "apiHeaderAll.hpp"
+
+// =-=-=-=-=-=-=-
+// STL Includes
+#include <iostream>
+
+extern int rsModAVUMetadata(rsComm_t *rsComm, modAVUMetadataInp_t *modAVUMetadataInp);
+
+extern "C" {
+
+    // =-=-=-=-=-=-=-
+    // 1. Write a standard issue microservice
+    int irods_hello(msParam_t *inPath, msParam_t *inAttr, msParam_t *inVal,
+                    msParam_t *inUnit, msParam_t* _out, ruleExecInfo_t* _rei ) {
+        std::string my_str = parseMspForStr(inAttr);
+
+	int returnCode;
+        char *outStr;
+	char *pathStr;
+	char *attrStr;
+	char *valStr;
+	char *unitStr;
+
+
+	/*************************************  USUAL INIT PROCEDURE **********************************/
+
+	pathStr = parseMspForStr (inPath);
+	attrStr = parseMspForStr (inAttr);
+	valStr = parseMspForStr (inVal);
+	unitStr = parseMspForStr (inUnit);
+	outStr = (char*)malloc(MAX_NAME_LEN);
+	snprintf(outStr, MAX_NAME_LEN, "Hello World!\nOur input string was: %s %s %s %s", pathStr, attrStr, valStr, unitStr);
+
+	fillStrInMsParam (_out, outStr);
+
+ 	modAVUMetadataInp_t modAVUMetadataInp;
+
+        memset(&modAVUMetadataInp, 0, sizeof (modAVUMetadataInp_t));
+        modAVUMetadataInp.arg0 = "add";
+        modAVUMetadataInp.arg1 = "-d"; /* -d for file */
+        modAVUMetadataInp.arg2 = pathStr; /* Do I need to malloc something new and copy it in? */
+        modAVUMetadataInp.arg3 = attrStr;
+        modAVUMetadataInp.arg4 = valStr;
+        modAVUMetadataInp.arg5 = unitStr;
+
+	returnCode = rsModAVUMetadata(_rei->rsComm, &modAVUMetadataInp);
+
+	return 0;
+    }
+
+    // =-=-=-=-=-=-=-
+    // 2.  Create the plugin factory function which will return a microservice
+    //     table entry
+    irods::ms_table_entry*  plugin_factory() {
+        // =-=-=-=-=-=-=-
+        // 3. allocate a microservice plugin which takes the number of function
+        //    params as a parameter to the constructor
+        irods::ms_table_entry* msvc = new irods::ms_table_entry( 5 );
+
+        // =-=-=-=-=-=-=-
+        // 4. add the microservice function as an operation to the plugin
+        //    the first param is the name / key of the operation, the second
+        //    is the name of the function which will be the microservice
+        msvc->add_operation( "irods_hello", "irods_hello" );
+
+        // =-=-=-=-=-=-=-
+        // 4. add the microservice function as an operation to the plugin
+        //    the first param is the name / key of the operation, the second
+        //    is the name of the function which will be the microservice
+        msvc->add_operation( "irods_hello2", "irods_hello2" );
+
+        // =-=-=-=-=-=-=-
+        // 5. return the newly created microservice plugin
+        return msvc;
+    }
+
+
+    // =-=-=-=-=-=-=-
+    // 1. Write a standard issue microservice
+    int irods_hello2(msParam_t *inPath, msParam_t *inAttr, msParam_t *inVal,
+                    msParam_t *inUnit, msParam_t* _out, ruleExecInfo_t* _rei ) {
+        std::string my_str = parseMspForStr(inAttr);
+
+        int returnCode;
+        char *outStr;
+        char *pathStr;
+        char *attrStr;
+        char *valStr;
+        char *unitStr;
+
+
+        /*************************************  USUAL INIT PROCEDURE **********************************/
+
+        pathStr = parseMspForStr (inPath);
+        attrStr = parseMspForStr (inAttr);
+        valStr = parseMspForStr (inVal);
+        unitStr = parseMspForStr (inUnit);
+        outStr = (char*)malloc(MAX_NAME_LEN);
+        snprintf(outStr, MAX_NAME_LEN, "Hello World 2!\nOur input string was: %s %s %s %s", pathStr, attrStr, valStr, unitStr);
+
+
+	return 0;
+	}
+
+}; // extern "C"
