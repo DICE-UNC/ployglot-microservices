c32fbcc6ae08479e062ac1bf0034e950966d39e8
Isaac Simmons <ids23@drexel.edu>
2014-09-18 16:32:14 -0400

    Removed subdirectory of test code

--- simple_curl/commandline.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-curl -F output_format=png -F file=@y18.gif -o commandline.png http://localhost:5000/ 
--- simple_curl/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-gcc `curl-config --cflags` -c main.c
-gcc -o main main.o `curl-config --libs`
-./main
